857a2a144992027a8dfad25f4233bb8a4aa43c7b
Yuriy <>
2023-07-12 00:57:45 +0300

    Initial commit

---
+++ CsharpHW2/task15/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 15: Напишите программу, которая принимает на 
+// вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int daynum = ReadInt("Введите число от 1 до 7: ");
+Console.WriteLine(WorkHoliday(daynum));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string WorkHoliday(int num)
+{
+    if(num > 0 & num < 8)
+    {
+        if(num == 7 || num == 6)
+        {
+            Console.Write($"Под цифрой {num} -> Выходной");
+        }
+        else
+        {
+            Console. Write($"Под цифрой {num} -> Будний");
+        }
+    }
+    else
+    {
+        Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому невозможно определить");
+    }
+    return " день.";
+}
---
+++ CsharpHW3/task21/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double Distance3D(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    int A = x2 - x1;
+    int B = y2 - y1;
+    int C = z2 - z1;
+    double lenght = Math.Sqrt(A * A + B * B + C * C);
+    return lenght;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = ReadInt("Введите координату X первой точки: ");
+int y1 = ReadInt("Введите координату Y первой точки: ");
+int z1 = ReadInt("Введите координату Z первой точки: ");
+int x2 = ReadInt("Введите координату X второй точки: ");
+int y2 = ReadInt("Введите координату Y второй точки: ");
+int z2 = ReadInt("Введите координату Z второй точки: ");
+
+Console.WriteLine(Distance3D(x1, y1, z1, x2, y2, z2));
